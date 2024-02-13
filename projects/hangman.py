ee9528201142bde3b1d3c558e22e2a57ce604d33
Shilpajordan <Shilpa.nair@dci-student.org>
2024-02-13 16:00:31 +0100

    hangman project start

---
+++ projects/hangman.py
@@ -0,0 +1,32 @@
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+    for i in range(word_length):
+        letter = chosen_word[i]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[i] = letter
+
+    print(display)
+
+    #Check if there are no more "_" left in 'display'. Then all letters have been guessed.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
