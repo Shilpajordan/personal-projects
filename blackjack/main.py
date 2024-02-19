51d9a382b689d9e28bfcc3058659a2e4b6a2d7ae
Shilpajordan <Shilpa.nair@dci-student.org>
2024-02-19 16:01:19 +0100

    new functions added, comparsion

--- blackjack/main.py
+++ blackjack/main.py
@@ -4,3 +4,37 @@ import os
 def clear():
     # Clear the console screen
     os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def deal_card():
+  """Returns a random card from the deck."""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+def calculate_score(cards):
+  """Take a list of cards and return the score calculated from the cards"""
+  if sum(cards) == 21 and len(cards) == 2:
+        return 0
+  if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+  return sum(cards)
+
+def compare(user_score, computer_score):
+    if user_score > 21 and computer_score > 21:
+        return "you went over . You lose 😤"
+    if user_score == computer_score:
+        return "Draw 🙃"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack 😱"
+    elif user_score == 0:
+        return "Win with a Blackjack 😎"
+    elif user_score > 21:
+        return "You went over. You lose 😭"
+    elif computer_score > 21:
+        return "Opponent went over. You win 😁"
+    elif user_score > computer_score:
+        return "You win 😃"
+    else:
+        return "You lose 😤"
